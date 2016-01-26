32ce0d98e8ec6e43619f6972754fd1c876f46483
Danilo Silva <badkill82@gmail.com>
2016-01-26 17:30:47 +0100

    completed 1.22

---
+++ 1.22.scm
@@ -0,0 +1,39 @@
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+    (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes from to)
+  (cond ((even? from)
+         (search-for-primes (+ from 1) to))
+        ((< from to)
+         (timed-prime-test from)
+         (search-for-primes (+ from 2) to))))
+
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(search-for-primes 1000000000000 1100000000000)
+; (search-for-primes 1 20)

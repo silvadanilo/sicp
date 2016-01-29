15e9e42e74c149bc66059d8d249766ca885d740a
Danilo Silva <badkill82@gmail.com>
2016-01-29 15:00:28 +0100

    completed 1.24

---
+++ 1.24.scm
@@ -0,0 +1,45 @@
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 20)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes from to)
+  (cond ((even? from)
+         (search-for-primes (+ from 1) to))
+        ((< from to)
+         (timed-prime-test from)
+         (search-for-primes (+ from 2) to))))
+
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(search-for-primes 1000000000000 1100000000000)
+; (search-for-primes 1 20)

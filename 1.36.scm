1adeba0e6107cf11263753ddbd80de0ac311521f
Danilo Silva <badkill82@gmail.com>
2016-02-08 15:49:25 +0100

    completed 1.36

---
+++ 1.36.scm
@@ -0,0 +1,21 @@
+(define tolerance 0.00001)
+(define (fixed-point f first-guess label)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display label)
+    (display " => ")
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(newline)
+(display (fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0 "without-average"))
+
+(newline) (newline)
+(display (fixed-point (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2)) 2.0 "with-average"))
+(newline)

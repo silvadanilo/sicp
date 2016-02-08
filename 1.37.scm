d1c2a98834c82b51cf5b488fbc4efe9fda194d4a
Danilo Silva <badkill82@gmail.com>
2016-02-08 17:03:21 +0100

    completed 1.37

---
+++ 1.37.scm
@@ -0,0 +1,25 @@
+(define (cont-frac n d k)
+  (if (= k 1)
+      (/ (n k) (d k))
+      (/ (n k) (+ (d k) (cont-frac n d (- k 1))))))
+
+(newline)
+(display "recursive " )
+(display (cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           11))
+
+(define (cont-frac-iter n d k)
+  (define (iter i result)
+    (if (= i 0)
+        result
+        (iter (- i 1) (/ (n i) (+ (d i) result)))))
+  (iter (- k 1) (/ (n k) (d k))))
+
+(newline)
+(newline)
+(display "iterative " )
+(display (cont-frac-iter (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           11))
+(newline)

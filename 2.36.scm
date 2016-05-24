f0985843eb44efbb136f90f42bcf65ab97ecd3df
SAMPLE <SAMPLE>
2016-05-24 19:58:26 +0200

    completed 2.36

---
+++ 2.36.scm
@@ -0,0 +1,15 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (lambda (seq) (car seq)) seqs))
+            (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
+
+(newline)
+(display (accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))) ; expects (22 26 30)

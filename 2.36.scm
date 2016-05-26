23012c47c0a69ccf027d6dc7038f201cc0edf37e
SAMPLE <SAMPLE>
2016-05-26 09:02:32 +0200

    improved 2.36 solution removing unuseful lambad

--- 2.36.scm
+++ 2.36.scm
@@ -8,8 +8,8 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       '()
-      (cons (accumulate op init (map (lambda (seq) (car seq)) seqs))
-            (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
 
 (newline)
 (display (accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))) ; expects (22 26 30)
